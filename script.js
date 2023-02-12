6066b7aa2c0546791afee7dddf47b3e445313edc
Ayrton Santillana <ayrtontv@gmail.com>
2023-02-12 13:16:43 -0600

    Se agregó función playRound que comienza el juego

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const armas = ["Espada", "Hacha", "Lanza"];
-
 function getComputerChoice(){
+    const armas = ["Espada", "Hacha", "Lanza"];
     return armas[Math.floor(Math.random() * armas.length)];
 }
 
-console.log(getComputerChoice());
+const playerSelection = "Espada";
+const computerSelection = getComputerChoice();
+
+console.log(computerSelection);
+
+function playRound(playerSelection, computerSelection){
+    if((playerSelection == "Espada" && computerSelection == "Hacha") ||
+    (playerSelection == "Hacha" && computerSelection == "Lanza") || 
+    (playerSelection == "Lanza" && computerSelection == "Espada")){
+        return `Ganaste! ${playerSelection} vence a ${computerSelection}`;
+    }
+    else if((playerSelection == "Hacha" && computerSelection == "Espada") ||
+    (playerSelection == "Lanza" && computerSelection == "Hacha") || 
+    (playerSelection == "Espada" && computerSelection == "Lanza")){
+        return `Perdiste, ${computerSelection} vence a ${playerSelection}`;
+    }
+    else if(playerSelection === computerSelection){
+        return `${playerSelection} VS ${computerSelection}! Es un EMPATE!`
+    }
+}
+
+console.log(playRound(playerSelection, computerSelection));

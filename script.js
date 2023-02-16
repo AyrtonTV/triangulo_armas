06f40b39194311ec91b1ba67215b65734b276c02
Ayrton Santillana <ayrtontv@gmail.com>
2023-02-16 00:27:00 -0600

    Sea agregó la función game() con bucle for para jugar 5 rondas, un contador de puntos y alertas en pantalla

--- script.js
+++ script.js
@@ -1,27 +1,72 @@
-function getComputerChoice(){
+const computerSelection = getComputerChoice();
+let jugador = prompt("¿Cuál es tu nombre?");
+
+let playerScore = 0;
+let computerScore = 0;
+let resultado;
+
+game();
+
+
+function getComputerChoice() {
     const armas = ["Espada", "Hacha", "Lanza"];
     return armas[Math.floor(Math.random() * armas.length)];
 }
 
-const playerSelection = "Espada";
-const computerSelection = getComputerChoice();
-
-console.log(computerSelection);
-
 function playRound(playerSelection, computerSelection){
-    if((playerSelection == "Espada" && computerSelection == "Hacha") ||
-    (playerSelection == "Hacha" && computerSelection == "Lanza") || 
-    (playerSelection == "Lanza" && computerSelection == "Espada")){
-        return `Ganaste! ${playerSelection} vence a ${computerSelection}`;
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    let playerSelectionTitle = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    let computerSelectionTitle = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+
+    if(playerSelection === computerSelection){
+        resultado = `${playerSelectionTitle} VS ${computerSelectionTitle}! Es un EMPATE!`;
     }
-    else if((playerSelection == "Hacha" && computerSelection == "Espada") ||
-    (playerSelection == "Lanza" && computerSelection == "Hacha") || 
-    (playerSelection == "Espada" && computerSelection == "Lanza")){
-        return `Perdiste, ${computerSelection} vence a ${playerSelection}`;
+    else if((playerSelection == "espada" && computerSelection == "hacha") ||
+        (playerSelection == "hacha" && computerSelection == "lanza") ||
+        (playerSelection == "lanza" && computerSelection == "espada")){
+            resultado =`Ganaste! ${playerSelectionTitle} vence a ${computerSelectionTitle}!`;
     }
-    else if(playerSelection === computerSelection){
-        return `${playerSelection} VS ${computerSelection}! Es un EMPATE!`
+    else{
+        resultado =`Perdiste! ${computerSelectionTitle} vence a ${playerSelectionTitle}`;
     }
+    return resultado;
 }
 
-console.log(playRound(playerSelection, computerSelection));
+console.log(playerScore);
+console.log(computerScore);
+
+function game(){
+    for(let i = 0; i < 5; i++){
+        let playerSelection = prompt("Elige entre Espada, Lanza o Hacha:").toLowerCase();
+        let resultado = playRound(playerSelection, getComputerChoice());
+
+        let playerSelectionTitle = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+        let computerSelectionTitle = resultado.split(" ")[3].charAt(0).toUpperCase() + resultado.split(" ")[3].slice(1);
+
+
+        if(resultado.includes("Ganaste")){
+            playerScore++;
+        }
+        else if(resultado.includes("Perdiste")){
+            computerScore++;
+        }
+        console.log(resultado.replace(playerSelection, playerSelectionTitle).replace(computerSelectionTitle, computerSelectionTitle));
+        alert(resultado.replace(playerSelection, playerSelectionTitle).replace(computerSelectionTitle, computerSelectionTitle));
+    }
+}
+console.log(`Puntajes: ${jugador}: ${playerScore}, Computadora: ${computerScore}`);
+alert(`Puntajes: ${jugador}: ${playerScore}, Computadora: ${computerScore}`);
+if(playerScore > computerScore) {
+    console.log('¡Felicitaciones! Ganaste el juego.');
+    alert("Felicidades, Ganaste el Juego");
+    } 
+    else if (playerScore < computerScore) {
+    console.log('Lo siento, perdiste el juego.');
+    alert("Lo siento, perdiste el juego :(");
+    } 
+    else {
+    console.log('¡Empate!');
+    alert("Empate!")
+    }
